dbcf425e082295632284656e856c3913827b66f1
bdbrwr <bart@bdbrwr.nl>
2025-04-23 23:45:53 +0200

    Added player class and render

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
-        for event in pygame.event.get():
+        for event in pygame.event.get():   
             if event.type == pygame.QUIT:
                 return
-        
-        
+            
         screen.fill("black")
 
-        #call last
-        pygame.display.flip()
+        player.draw(screen)
+
+        pygame.display.flip() #call last
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
